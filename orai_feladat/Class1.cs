90271ba71b52414895932990920b1d59d3d46ba1
Petrik Diak <diak@petrik.hu>
2025-02-07 11:17:44 +0100

    konyv nevu osztaly letrehozva. 2 konstruktor es 3 metodus

--- orai_feladat/Class1.cs
+++ orai_feladat/Class1.cs
@@ -6,7 +6,60 @@ using System.Threading.Tasks;
 
 namespace orai_feladat
 {
-	internal class Class1
+	internal class Konyv
 	{
+		private string cim;
+		private string szerzo;
+		private int kiadasEve;
+		private int oldalszam;
+		private int ar;
+
+		public string Cim { get => cim; set => cim = value; }
+		public string Szerzo { get => szerzo; set => szerzo = value; }
+		public int KiadasEve { get => kiadasEve; set => kiadasEve = value; }
+		public int Oldalszam { get => oldalszam; set => oldalszam = value; }
+		public int Ar { get => ar; set => ar = value; }
+
+		public Konyv(string cim, string szerzo, int kiadasEve, int oldalszam, int ar)
+		{
+			this.cim = cim;
+			this.szerzo = szerzo;
+			this.kiadasEve = kiadasEve;
+			this.oldalszam = oldalszam;
+			this.ar = ar;
+
+
+		}
+
+		public Konyv(string cim, string szerzo)
+		{
+			this.cim = cim;
+			this.szerzo = szerzo;
+			kiadasEve = DateTime.Now.Year;
+			oldalszam = 200;
+			ar = 300;
+		}
+
+		public void Arnoveles(int osszeg)
+		{
+			ar += osszeg;
+		}
+
+		public void Kedvezmeny(double szazalek)
+		{
+			ar -= Convert.ToInt16(ar * szazalek / 100);
+
+		}
+
+		public int HanyOldal(int elolvasott)
+		{
+			return oldalszam = elolvasott;
+		}
+
+		public override string ToString()
+		{
+			return $"Konyv cime:{this.cim},Konyv szerzo: {this.szerzo},Kiadas eve: {this.kiadasEve},Oldalszam: {this.oldalszam},Konyv ar: {this.ar}";
+		}
+
 	}
 }
--- orai_feladat/Program.cs
+++ orai_feladat/Program.cs
@@ -10,6 +10,16 @@ namespace orai_feladat
 	{
 		static void Main(string[] args)
 		{
+			Konyv konyv1 = new Konyv("A kis herceg", "valaki",1943,96,2500);
+			Konyv konyv2 = new Konyv("Ez meg keszul","En");
+
+			Console.WriteLine(konyv1.ToString() + "\n" + konyv2.ToString());
+
+			konyv1.Kedvezmeny(20);
+
+			konyv1.Arnoveles(1000);
+
+			Console.WriteLine("\n" + konyv1.ToString() + "\n" + konyv1.HanyOldal(20));
 		}
 	}
 }
